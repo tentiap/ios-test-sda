b7f0f5198cd5e5fb0eb71010c1792eaa4a974c04
tentiap <inooyabu@gmail.com>
2022-10-15 14:22:29 +0700

    initial commit

---
+++ ios-test-sda/Controller/LoginScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginScreenViewController.swift
+//  ios-test-sda
+//
+//  Created by Tenti Atika Putri on 15/10/22.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
